46ceccac9a9e5631ea356db6d6681562745d1303
Carolyn Jankowski <carolyn.j.jankowski@gmail.com>
2023-03-27 19:55:34 -0400

    refactored code library

--- main.py
+++ main.py
@@ -1,8 +1,8 @@
-import data as du
-import general_utility as gu
+from PTLIB import general_utility as gu
 
+#df, instruments = du.get_sp500_df()
+# df = du.extend_dataframe(traded=instruments, df=df)
 
-df, instruments = du.get_sp500_df()
-df = du.extend_dataframe(traded=instruments, df=df)
-
-gu.save_file("./Data/data.obj", (df, instruments))
+#gu.save_file("./Data/data.obj", (df, instruments))
+df, instruments = gu.load_file("./Data/data.obj")
+print(df, instruments)
---
+++ setup.py
@@ -0,0 +1,13 @@
+import setuptools
+
+setuptools.setup(
+    name="ptlib",
+    version="0.1",
+    description="cod lib by Carolyn Jankowski",
+    url="#",
+    author="Carolyn Jankowski",
+    install_requires=["opencv-python"],
+    author_email="",
+    packages=setuptools.find_packages(),
+    zip_safe=False
+)
