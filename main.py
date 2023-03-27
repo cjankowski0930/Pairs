ccf3f14d71072531f12bee395e6c77670672d612
Carolyn Jankowski <carolyn.j.jankowski@gmail.com>
2023-03-27 19:02:16 -0400

    added imports and save to pickle

--- main.py
+++ main.py
@@ -1,5 +1,8 @@
-from data import get_sp500_df
-
+from data import get_sp500_df as du
+import general_utility as gu
 
 if __name__ == '__main__':
-    df, instruments = get_sp500_df()
+    df, instruments = du.get_sp500_df()
+    df = du.extend_dataframe(traded=instruments, df=df)
+
+    gu.save_file("./Data/data.obj", (df, instruments))
