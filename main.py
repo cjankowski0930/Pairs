6d024241d67c2172315ca6134ab56daecaa6b660
Carolyn Jankowski <carolyn.j.jankowski@gmail.com>
2023-03-27 19:06:43 -0400

    modified import statement

--- main.py
+++ main.py
@@ -1,8 +1,8 @@
-from data import get_sp500_df as du
+import data as du
 import general_utility as gu
 
-if __name__ == '__main__':
-    df, instruments = du.get_sp500_df()
-    df = du.extend_dataframe(traded=instruments, df=df)
 
-    gu.save_file("./Data/data.obj", (df, instruments))
+df, instruments = du.get_sp500_df()
+df = du.extend_dataframe(traded=instruments, df=df)
+
+gu.save_file("./Data/data.obj", (df, instruments))
