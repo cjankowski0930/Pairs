7e818928e23f4ff22411eef09bfbd234329fd094
Carolyn Jankowski <carolyn.j.jankowski@gmail.com>
2023-03-28 22:11:03 -0400

    added logic

--- main.py
+++ main.py
@@ -3,7 +3,7 @@ from PTLIB import data as du
 import random
 import json
 from dateutil.relativedelta import relativedelta
-from subsystems.LBMOM.subsys import LBMOM
+import subsystems.LBMOM.subsys
 
 #df, instruments = du.get_sp500_df()
 # df = du.extend_dataframe(traded=instruments, df=df)
@@ -16,4 +16,4 @@ VOL_TARGET = 0.20
 print(df.index[-1])
 sim_start = df.index[-1] - relativedelta(years=5)
 print(sim_start)
-strat = LBMOM(instruments_config='./subsystems/LBMOM/config.json', historical_df=df, simulation_start=sim_start, vol_target=VOL_TARGET)
+strat = subsystems.LBMOM.subsys.LBMOM(instruments_config='./subsystems/LBMOM/config.json', historical_df=df, simulation_start=sim_start, vol_target=VOL_TARGET)
